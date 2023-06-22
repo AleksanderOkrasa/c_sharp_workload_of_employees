954065721831723702e4838382d76f156f209c7e
AleksanderOkrasa <95458102+AleksanderOkrasa@users.noreply.github.com>
2023-06-22 16:39:05 +0200

    Stworzenie wstępnego (jeszcze nie w pełni funkcjonalnego) panelu do dodawania zadań

--- WebAPI/WpfApp/ViewModel/WorkloadViewModel.cs
+++ WebAPI/WpfApp/ViewModel/WorkloadViewModel.cs
@@ -1,12 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp.Basic;
+using WpfApp.Model;
 
 namespace WpfApp.ViewModel
 {
-    internal class WorkloadViewModel
+    internal partial class WorkloadViewModel : ViewModelBase
     {
+        public ObservableCollection<TaskModel> Tasks { get; set; } // Przechowywanie zadań
+
+        private string newTaskDescription;
+        public string NewTaskDescription
+        {
+            get { return newTaskDescription; }
+            set
+            {
+                if (newTaskDescription != value)
+                {
+                    newTaskDescription = value;
+                    OnPropertyChanged("NewTaskDescription");
+                }
+            }
+        }
+
+        public Command AddTaskCommand { get; private set; }
+
+        public WorkloadViewModel()
+        {
+            Tasks = new ObservableCollection<TaskModel>();
+
+            AddTaskCommand = new Command(AddTask);
+        }
+
+        private void AddTask()
+        {
+            TaskModel newTask = new TaskModel
+            {
+                ID = GenerateNewTaskID(),
+                TaskDescription = NewTaskDescription,
+            };
+
+            Tasks.Add(newTask);
+            NewTaskDescription = string.Empty;
+        }
+
+        private int GenerateNewTaskID()
+        {
+            if (Tasks.Count > 0)
+                return Tasks[Tasks.Count - 1].ID + 1;
+            else
+                return 1;
+        }
     }
 }
