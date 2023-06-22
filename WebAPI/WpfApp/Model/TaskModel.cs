c7c9e491599537f1e5adb898a7323842d96c6e74
AleksanderOkrasa <95458102+AleksanderOkrasa@users.noreply.github.com>
2023-06-22 15:52:03 +0200

    Dodanie pomocniczych modeli pracownika i zadania (umożliwiające rozwijanie kodu przed integracją z webapi)

---
+++ WebAPI/WpfApp/Model/TaskModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Model
+{
+    internal class TaskModel
+    {
+        public int ID { get; set; }
+        public string TaskDesctription { get; set; }
+        public int EmployeeId { get; set; }
+        public double Time { get; set; }
+        public int Priority { get; set; }
+    }
+}
