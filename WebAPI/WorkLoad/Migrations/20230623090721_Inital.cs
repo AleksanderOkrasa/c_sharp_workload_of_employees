b201f33412285439213b5483370a60f12fee4bb4
AleksanderOkrasa <95458102+AleksanderOkrasa@users.noreply.github.com>
2023-06-23 13:04:37 +0200

    .

---
+++ WebAPI/WorkLoad/Migrations/20230623090721_Inital.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkLoad.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inital : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    description = table.Column<string>(type: "TEXT", nullable: false),
+                    priority = table.Column<int>(type: "INTEGER", nullable: false),
+                    time = table.Column<int>(type: "INTEGER", nullable: false),
+                    isDone = table.Column<bool>(type: "INTEGER", nullable: false),
+                    employeeID = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tasks",
+                columns: new[] { "Id", "description", "employeeID", "isDone", "priority", "time" },
+                values: new object[] { 1, "Defekacja", null, false, 5, 1 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
---
+++ WebAPI/WorkLoad/Task.cs
@@ -0,0 +1,13 @@
+﻿namespace WorkLoad
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string description { get; set; }
+        public int priority { get; set; }
+        public int time { get; set; }
+        public bool isDone { get; set; }
+        public int? employeeID {get; set; }
+        public Employee? employee { get; set; }
+    }
+}
