6dfcc55ba16fc3e6c1c4e399b121244d454d225f
AleksanderOkrasa <95458102+AleksanderOkrasa@users.noreply.github.com>
2023-07-01 16:17:03 +0200

    Stworzenie nowego projektu - Workload. Widoki przekopiowane z projektu WpfApp, klasa w folderze Basic i klasy Widoków Modeli (w ViewsModels) dziedziczące po klasach z projektu WpfApp

---
+++ Workload/ViewModel/EmployeeManagerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Workload.ViewModel
+{
+    internal class EmployeeManagerViewModel : WpfApp.ViewModel.EmployeeManagerViewModel
+    {
+        public EmployeeManagerViewModel(WpfApp.ViewModel.WorkloadViewModel workloadViewModel) : base(workloadViewModel)
+        {
+        }
+    }
+}
