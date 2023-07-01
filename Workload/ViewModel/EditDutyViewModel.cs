19219259c68ab8b192373e7443c420224991008d
AleksanderOkrasa <95458102+AleksanderOkrasa@users.noreply.github.com>
2023-07-01 23:51:00 +0200

    pierwsze połączenie datacontext widoku z modelem widoku

--- Workload/ViewModel/EditDutyViewModel.cs
+++ Workload/ViewModel/EditDutyViewModel.cs
@@ -12,12 +12,29 @@ namespace Workload.ViewModel
     {
         private DutyModel _selectedDuty;
 
+        private string editedDutyDescription;
+        private int editedPriority;
+        private KeyValuePair<int, string> _editedPriority;
+        private double editedTimeValue;
+        private int editedEmployeeID;
+
+
         public EditDutyViewModel(DutyModel selectedDuty)
         {
-            this._selectedDuty = selectedDuty;
+            editedDutyDescription = selectedDuty.DutyDescription;
+            editedPriority = selectedDuty.Priority;
+            editedTimeValue = selectedDuty.Time;
+            editedEmployeeID = selectedDuty.EmployeeId;
         }
 
+        public string EditedDutyDescription { get => editedDutyDescription; set => Set(ref editedDutyDescription, value); }
+        public int EditedPriority { get => editedPriority; set => Set(ref editedPriority, value); }
+        public KeyValuePair<int, string> EditedPriorityDict { get => _editedPriority; set => Set(ref _editedPriority, value); }
+        public double EditedTimeValue { get => editedTimeValue; set => Set(ref editedTimeValue, value); }
+        public int EditedEmployeeID { get => editedEmployeeID; set => Set(ref editedEmployeeID, value); }
+
         public DutyModel SelectedDuty { get => _selectedDuty; set => Set(ref _selectedDuty, value); }
+        public Command EditDutyCommand { get; set; }
     }
 
 }
